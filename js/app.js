b7267c2389602a4f635286d74336ce22e2b23aba
trevorstam <trevorstam@gmail.com>
2018-07-11 15:18:35 -0700

    changed question to answer.

--- js/app.js
+++ js/app.js
@@ -6,12 +6,12 @@ console.log('userName', nameAnswer);
 alert('Welcome '+ nameAnswer + 'let\'s start the guessing game!');
 
 // Was I born abroad?
-var questionOne = prompt('Was I born abroad? (Y/N)');
-console.log('Was I born abroad?', questionOne);
-if (questionOne.toUpperCase() === 'Y') {
+var answerOne = prompt('Was I born abroad? (Y/N)');
+console.log('Was I born abroad?', answerOne);
+if (answerOne.toUpperCase() === 'Y') {
   alert('True! I was born in the Netherlands, grew up there and finished my bachelor\'s'+
         'degree and master\'s degree in Geography');
-} else if (questionOne.toUpperCase() === 'N') {
+} else if (answerOne.toUpperCase() === 'N') {
   alert('False! I was born in the Netherlands, grew up there and finished my bachelor\'s' +
         'degree and master\'s degree in Geography');
 } else {
@@ -19,45 +19,47 @@ if (questionOne.toUpperCase() === 'Y') {
 }
 
 //Am I eligible to work in the United States?
-var questionTwo = prompt('Am I eligible to work in the United States? (Y/N)');
-console.log('Am I eligible to work in the United States?', questionTwo);
-if (questionTwo.toUpperCase() === 'Y'){
+var answerTwo = prompt('Am I eligible to work in the United States? (Y/N)');
+console.log('Am I eligible to work in the United States?', answerTwo);
+if (answerTwo.toUpperCase() === 'Y'){
   alert('True! I have American citizenship!');
-} else if (questionTwo.toUpperCase() === 'N') {
+} else if (answerTwo.toUpperCase() === 'N') {
   alert('Wrong! I have American citizenship');
 } else {
   alert('Try again');
 }
 
 //Do I have 10 years of professional experience?
-var questionThree = prompt('Do I have 10 years of professional experience? (Y/N)');
-console.log('Do I have 10 years of professional experience?', questionThree);
-if (questionThree.toUpperCase() === 'Y') {
+var answerThree = prompt('Do I have 10 years of professional experience? (Y/N)');
+console.log('Do I have 10 years of professional experience?', answerThree);
+if (answerThree.toUpperCase() === 'Y') {
   alert('True! I have worked as an economic researcher and GIS Analyst before.');
-} else if (questionThree.toUpperCase() ==='N') {
+} else if (answerThree.toUpperCase() ==='N') {
   alert('Incorrect. I have worked as an economic researcher and GIS Analyst before.');
 } else {
   alert('Try again');
 }
 
 //Would I like to become a great software developer?
-var questionFour = prompt('Would I like to find a job in software development?(Y/N)');
-console.log('Would I like to find a job in software development?', questionFour);
-if (questionFour.toUpperCase() === 'Y') {
+var answerFour = prompt('Would I like to find a job in software development?(Y/N)');
+console.log('Would I like to find a job in software development?', answerFour);
+if (answerFour.toUpperCase() === 'Y') {
   alert('Yes, I find it a fascinating field');
-} else if (questionFour.toUpperCase() === 'N') {
+} else if (answerFour.toUpperCase() === 'N') {
   alert('Incorrect. The answer is yes!');
 } else {
   alert('try again');
 }
 //Do I like building birdhouses?
 
-var questionFive = prompt('Do I like building birdhouses? (Y/N)');
-console.log('Do I like building birdhouses?', questionFive);
-if (questionFive.toUpperCase() === 'Y') {
+var answerFive = prompt('Do I like building birdhouses? (Y/N)');
+console.log('Do I like building birdhouses?', answerFive);
+if (answerFive.toUpperCase() === 'Y') {
   alert('Are you serious???');
-} else if (questionFive.toUpperCase() === 'N') {
+} else if (answerFive.toUpperCase() === 'N') {
   alert('Correct! Birdhouses are quite useless. However I do play gypsy and jazz violin. I also like hiking and reading.');
 } else {
   alert('Try again');
 }
+
+//What is my favorite number?
